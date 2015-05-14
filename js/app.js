d28e970578cd4aafc6f09de1bd3210612bb063ea
Dara <daradosu@gmail.com>
2015-05-14 18:39:34 +0100

    Edit functionality implemented

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 var eggHeadApp = angular.module('egghead', ['LocalStorageModule','ngSanitize']);
+
+
+
+$(document).ready(function(){
+  $('#addNewVideo').click(function(){
+    $('#addVideoDiv,#addVideoBackDrop').toggle(500);
+  });
+
+  $('#addVideoButton').click(function(){
+    $('#addVideoDiv,#addVideoBackDrop').toggle(500);
+  });
+
+  $('.editButton').click(function(){
+    $('#editVideoDiv,#editVideoBackDrop').toggle(500);
+  });
+
+  $('#saveVideoButton').click(function(){
+    $('#editVideoDiv,#editVideoBackDrop').toggle(500);
+  });
+
+  $('.cancelEditVideo').click(function(){
+    $('#editVideoDiv,#editVideoBackDrop').toggle(500);
+  });
+
+  $('.cancelAddButton').click(function(){
+    $('#addVideoDiv,#addVideoBackDrop').toggle(500);
+  });
+
+});
--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -9,7 +9,6 @@
     }
   }
 
-  getAllVideos();
 
   assignId = function(){
     if(last_key == undefined){
@@ -20,11 +19,18 @@
     id = last_key;
   };
 
+  getAllVideos();
+
   $scope.clearAll = function(){
     localStorageService.clearAll();
   };
 
   $scope.addVideo = function(){
+    $('.addInput').val("");
+    console.log('adding a video');
+    submittedLink = $scope.videoLink;
+    videoAddress = submittedLink.split('=')[1];
+    newVideoLink = 'https://www.youtube.com/embed/'+videoAddress;
 
     prevKeys = localStorageService.keys().sort(function(a,b){return a-b;});//get existing keys (array)
     last_key = prevKeys[prevKeys.length - 1];//select last key (number)
@@ -34,13 +40,14 @@
     video = {
       videoId : id,
       title : $scope.videoTitle,
-      link : $scope.videoLink,
+      link : newVideoLink,
       likes : 0
     } 
 
     if(id == 0){//no previous video in localstorage, currrent entry cant be a duplicate
       localStorageService.set(id, video);
       $scope.videoLinks.push(video);
+      console.log('video added');
     }else{
       lastEntry = localStorageService.get(id-1);//get details of the last entry (object)
       if(lastEntry.link == video.link && lastEntry.title == video.title){
@@ -48,51 +55,62 @@
       }else{
         localStorageService.set(id, video);
         $scope.videoLinks.push(video);
+        console.log('video added');
       }
-    }
-    //getAllVideos();
-    
-  }
+    } 
+
+
+  };
 
   $scope.deleteVideo = function(id){
+    $scope.text = 'A new  paragraph';
     videosArray = $scope.videoLinks;
     localStorageService.remove(id);
     for(i = 0; i < videosArray.length ; i++){
       if($scope.videoLinks[i].videoId == id){
-        $scope.videoLinks.splice(i,1);
+        $scope.videoLinks.splice(i, 1);
       }
     }
-  }  
+  };
 
   $scope.likeVideo = function(id){
     likedVid = localStorageService.get(id);
-    console.log(id);
     likedVid.likes += 1;
     localStorageService.remove(id);
     localStorageService.set(id, likedVid);
-    //$scope.videoLinks[id].likes += 1;
     videosArray = $scope.videoLinks;
     for(i = 0; i < videosArray.length ; i++){
       if(videosArray[i].videoId == id){
         $scope.videoLinks[i].likes += 1;
       }
     }
-    console.log(videosArray);
   };
 
-  $scope.actionKey = function(){
-    prevKeys = localStorageService.keys();
-    last_key = prevKeys[prevKeys.length - 1];
-    assignId();
-    lastEntry = localStorageService.get(id-1);
-  }  
+  $scope.editVideoLink = '';
+  $scope.editVideoTitle = '';
+  $scope.editVideoId = '';
 
+  $scope.editAVideo = function(id){
+    oldVid = localStorageService.get(id);
+    $scope.editVideoLink = oldVid.link;
+    $scope.editVideoTitle = oldVid.title;
+    $scope.editVideoId = id;
 
- // function submit(key, val) {
-   //return
+  };
 
-  //}
+  $scope.saveEditedVideo = function(){
+    id = $scope.editVideoId;
+    console.log(id);
+    videoToEdit = localStorageService.get(id);
+    videoToEdit.title = $scope.editVideoTitle;
+    videoToEdit.link = $scope.editVideoLink;
+    localStorageService.remove(id);
+    localStorageService.set(id, videoToEdit);
+  };
+
+  
  
+
 }])
 
 .filter('trusted', ['$sce', function ($sce) {
@@ -100,3 +118,30 @@
         return $sce.trustAsResourceUrl(url);
     };
 }]);
+
+/*<!--<div ng-app="eggHeadApp">
+  <div ng-controller="MainCtrl">
+    <div id="addAVideo" class="modal fade" role="dialog" >
+      <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Add a Video</h4>
+          </div>
+          <div class="modal-body">
+            <form ng-submit="addVideo()">
+              Title: <input type="text" name="title" ng-model="videoTitle"><br/><br/>
+              Link: <input type="text" name="title" ng-model="videoLink"><br/><br/>
+          </div>
+          <div class="modal-footer">
+            <input type="button" class="btn btn-success"  value="submit">
+            <input type="submit" value="add">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div><!--add a video ends-->
+  </div>
+</div>*/
